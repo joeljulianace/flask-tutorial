721827726fd9e3db6e3b6378e983c15080c45dab
silktreesolutions <joel.r.julian0408@gmail.com>
2019-05-18 13:28:56 +0530

    Changed queries for postgres db

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,17 +1,30 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS post;
 
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
 CREATE TABLE post (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    author_id INTEGER NOT NULL,
+    id SERIAL PRIMARY KEY,
+    author_id INTEGER REFERENCES users NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES user (id)
+    body TEXT NOT NULL
 );
+
+
+-- CREATE TABLE flights(
+--     id SERIAL PRIMARY KEY,
+--     origin VARCHAR NOT NULL,
+--     destination VARCHAR NOT NULL,
+--     duration INTEGER NOT NULL
+-- );
+
+-- CREATE TABLE passengers(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR NOT NULL,
+--     flight_id INTEGER REFERENCES flights
+-- );
