beb37b8ba86483ee2183ed085a02b9869d18d72a
silktreesolutions <joel.r.julian0408@gmail.com>
2019-05-18 13:29:19 +0530

    made changes to connect to postgres

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,15 +3,13 @@ import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],  
-            detect_types = sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
+        engine = create_engine("postgresql://postgres:admin@localhost:5432/blogs")
+        g.db = scoped_session(sessionmaker(bind=engine))
     return g.db
 
 def close_db(e=None):
@@ -24,7 +22,8 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+        db.execute(f.read().decode('utf8'))
+    db.commit()
 
 @click.command('init-db')
 @with_appcontext
